b2fe000be950c7e323f217d54e3b2164b4eb4473
dancechae <ganyumedeth@naver.com>
2025-05-02 07:18:38 +0000

    Deploying to gh-pages from @ dancechae/dancechae.github.io@67156376803f5c191f30aa6730696b1733b298cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,16 +28,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-2025년-신진예술-지원사업-선정-세종시문화관광재단",
+          title: '2025년 신진예술 지원사업 선정(세종시문화관광재단)',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
