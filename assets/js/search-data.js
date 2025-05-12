e7d177fd57998b6d18fd41bcdc1a12f644df2bf6
dancechae <ganyumedeth@naver.com>
2025-05-12 15:08:50 +0000

    Deploying to gh-pages from @ dancechae/dancechae.github.io@78494069e03a6995109c136b8b6849038caf69f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,11 +53,6 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
             },},{id: "projects-고풍",
           title: '고풍',
           description: "한국 무용가 최현 선생님이 안무한 춤",
